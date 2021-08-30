5b2ec966acff309b05bff40a885596a1307ad46e
hasegawa-101 <hasegawa@101de-sign.com>
2021-08-31 01:27:35 +0900

    🔥 firebaseと連携

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,6 +4,8 @@ import { VFC } from 'react';
 import { AppProps } from 'next/app';
 import 'what-input';
 
+import '@src/lib/firebase'
+
 const MyApp: VFC<AppProps> = ({ Component, pageProps }) => {
   return <Component {...pageProps} />;
 };
